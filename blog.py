8aebce5e6b9ecffd40c95875446ad47a3cda0712
alihnbb <alihnbb@gmail.com>
2021-01-04 19:19:04 +0300

    navbar+todo

--- blog.py
+++ blog.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
-from flask_mysqldb import MySQL
-from wtforms import Form,StringField,PasswordField,validators
-from passlib.hash import sha256_crypt
+#from flask_mysqldb import MySQL
+#from wtforms import Form,StringField,PasswordField,validators
+#from passlib.hash import sha256_crypt
 from flask_sqlalchemy import SQLAlchemy
 
 
